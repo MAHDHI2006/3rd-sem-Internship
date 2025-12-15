7d8b38ef74680f0180997fb2d3543ca587a0b8b4
Shathigul Mahdhi <shathigulmahdhi710@gmail.com>
2025-12-15 14:11:09 +0530

    sixteen commit

---
+++ SampleExample/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/functionalComponents/Navbar'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    </Navbar>
+  )
+}
+
+export default App
