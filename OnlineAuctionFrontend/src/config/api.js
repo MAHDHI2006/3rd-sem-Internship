1aecb40b32af8c15ccfb0832ef9af6827a1bceee
MAHDHI2006 <shathigulmahdhi710@gmail.com>
2026-01-31 11:21:33 +0800

    Update API URL to new Render backend

--- OnlineAuctionFrontend/src/config/api.js
+++ OnlineAuctionFrontend/src/config/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://online-auction-system-kpuv.onrender.com';
+const API_BASE_URL = 'https://threerd-sem-internship-8.onrender.com';
 
 console.log('API_BASE_URL:', API_BASE_URL);
 
